312a642923ca664b658f6c02b39432e047f2abe9
Antoine Nattier <antoinenattier@macbook-pro-de-antoine.home>
2021-02-03 08:53:44 +0100

    nouvelle version

--- push_that.sh
+++ push_that.sh
@@ -4,15 +4,7 @@ cd /rendu/Piscine-J1
 
 git add .
 
-
-DATE=$(date)
-
-git commit -m "version du $DATE" 
+git commit -m "nouvelle version" 
 
 git pull
 
-osascript -e "display notification 'pushed to remote' with title 'SUCCESS'"
-
-
-
-rsync -r . /var/www/html --exclude .git
